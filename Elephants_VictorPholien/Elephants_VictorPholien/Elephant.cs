7981ece72510d174daa26131b552061ee835d388
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-11-07 14:17:18 +0100

    Ajout de l'elephant

---
+++ Elephants_VictorPholien/Elephants_VictorPholien/Elephant.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elephants
+{
+    class Elephant
+    {
+        private string _nom;
+        private uint _tailleOreilles;
+
+        public Elephant(string nom, uint tailleOreilles)
+        {
+            _nom = nom;
+            _tailleOreilles = tailleOreilles;
+        }
+
+        public string AfficherQuiJeSuis()
+        {
+            return _nom + " et j'ai des oreilles de " + _tailleOreilles.ToString() + " cm";
+        }
+
+        public uint GetEar()
+        {
+            return _tailleOreilles;
+        }
+
+        public void EcouteMessage(string message, Elephant quiDit)
+        {
+            Console.WriteLine(_nom + " a entendu un message \n" + quiDit._nom + " a dit : " + message);
+        }
+
+
+        public void EnvoieMessage(string message, Elephant quiRecoit)
+        {
+            quiRecoit.EcouteMessage(message, this);
+        }
+
+    }
+}
---
+++ Elephants_VictorPholien/Elephants_VictorPholien/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Elephants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant[] Elephants = new Elephant[2];
+            Elephants[0] = new Elephant("Zazou", 100);
+            Elephants[1] = new Elephant("Titi", 120);
+
+            Elephant[] LotElephants = new Elephant[7];
+            LotElephants[0] = new Elephant("1", 100);
+            LotElephants[1] = new Elephant("2", 80);
+            LotElephants[2] = new Elephant("3", 30);
+            LotElephants[3] = new Elephant("4", 180);
+            LotElephants[4] = new Elephant("5", 900);
+            LotElephants[5] = new Elephant("6", 30);
+            LotElephants[6] = new Elephant("7", 1);
+
+            Console.WriteLine("Choisissez \n 1 pour afficher les infos de zazou \n 2 pour titi \n 3 pour les echanger \n 4 pour vois le transfer d'un message de zazou à tit \n 5 pour reviser la notion de tableau et l'appliquer aux objets");
+            string choix = Console.ReadLine();
+
+            ChoixElephant(choix, Elephants, LotElephants);
+            Console.ReadLine();
+        }
+
+        static Elephant GetBiggestEar(Elephant[] LotElephants)
+        {
+            uint BiggestEar = 0;
+            int IndexElephant = 0;
+            for (int i = 0; i < LotElephants.Length; i++)
+            {
+                if (BiggestEar < LotElephants[i].GetEar())
+                {
+                    BiggestEar = LotElephants[i].GetEar();
+                    IndexElephant = i;
+                }
+            }
+
+            return LotElephants[IndexElephant];
+        }
+
+        static void ChoixElephant(string choix, Elephant[] Elephants, Elephant[] LotElephants)
+        {
+            string phrase = "je suis ";
+            switch (choix)
+            {
+                case "1":
+                    phrase += Elephants[0].AfficherQuiJeSuis();
+                    break;
+
+                case "2":
+                    phrase += Elephants[1].AfficherQuiJeSuis();
+                    break;
+
+                case "3":
+                    Elephant cacheElephant = Elephants[0];
+                    Elephants[0] = Elephants[1];
+                    Elephants[1] = cacheElephant;
+                    phrase += Elephants[0].AfficherQuiJeSuis();
+                    phrase += "\n";
+                    phrase += Elephants[1].AfficherQuiJeSuis();
+                    break;
+
+                case "4":
+                    Elephants[1].EnvoieMessage("salut", Elephants[0]);       
+                    phrase = null;
+                    break;
+
+                case "5":
+                    phrase += GetBiggestEar(LotElephants).AfficherQuiJeSuis();
+                    break;
+            }
+
+            Console.WriteLine(phrase);
+        }
+    }
+}
